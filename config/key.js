93e71feda3be1bc65fc9180e61dc87734e6ce01a
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2022-01-27 00:39:15 +0900

    비밀번호 암호화 설정

--- config/key.js
+++ config/key.js
@@ -1,5 +1,9 @@
 if(process.env.NODE_ENV==='production'){
+  //process.env.NODE_ENV 환경변수
+  //배포한 상태이면
   module.exports=require('./prod');
-}else{
+}
+  //로컬 상태이면
+else{
   module.exports=require('./dev');
 }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //몇자리 인지 설정
+
 
 const userSchema = mongoose.Schema({
   name:{
@@ -30,6 +33,26 @@ const userSchema = mongoose.Schema({
     type:Number
   }
 })
+
+//mongoose에서 가져오는 메소드
+//index.js에서 save하기전에 진행
+userSchema.pre('save',function(next){
+  var user = this;
+  //비밀번호가 변경이 될때만 암호화
+  if(user.isModified('password')){
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+        // Store hash in your password DB.
+      });
+    });
+  }
+  
+});
 
 const User=mongoose.model('User',userSchema)
 
