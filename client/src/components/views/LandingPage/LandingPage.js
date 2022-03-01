16cabfbc4b8de7c7aecb700301515e65d0ed871e
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2022-03-01 21:15:36 +0900

    logout 부분

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,35 @@
 import React,{useEffect} from 'react'
 import axios from 'axios';
+import {useNavigate} from "react-router-dom";
 
 function LandingPage(){
+  let navigate=useNavigate();
+
   useEffect(()=>{
     axios.get('/api/hello')
     .then(response=>{console.log(response)})
   })
+
+  const onClickHandler=()=>{
+    axios.get('/api/user/logout')
+      .then(response=>{
+        if(response.data.success){
+          navigate('/')
+        }else{    
+          alert('logout Error')
+        }}
+      )
+  }
   return(
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width:'100%', height:'100vh'
     }}>
       <h2>시작 페이지</h2>
+      <br></br>
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   )
 }
